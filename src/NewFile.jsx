d09142b4f0187f07e0b8fb7c1533b2c289c2dac9
junaidarshad1122 <jj8286014@gmail.com>
2025-10-24 23:31:20 -0700

    text

--- src/NewFile.jsx
+++ src/NewFile.jsx
@@ -4,6 +4,7 @@ const NewFile = () => {
   return (
     <div>
         <h1>Abubakar Aslam</h1>
+        <h2>Junaid Arshad</h2>
     </div>
   )
 }
--- src/components/File.jsx
+++ src/components/File.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const File = () => {
 return (
     <div>
+        <button type='button'>push</button>
 <h1>Practice Git Page</h1>
 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, sed, saepe porro voluptates, explicabo repellendus amet facere dolores necessitatibus harum adipisci rerum veniam excepturi praesentium eos distinctio eaque labore qui.</p>
     </div>
