b2e86dc1ceeb571940e9fe70fa383de2b2e1dbb8
junaidarshad1122 <jj8286014@gmail.com>
2025-10-24 23:49:06 -0700

    jjj

--- src/NewFile.jsx
+++ src/NewFile.jsx
@@ -5,11 +5,9 @@ const NewFile = () => {
     <div>
         <h1 style={{backgroundColor:"green"}}>Abubakar Aslam</h1>
         <h1 style={{fontSize:"56px", color:"purple"}}> Muhammad Abdullah Saeed</h1>
-<<<<<<< HEAD
         <h1 style={{color:"pink"}}>Git Push Something </h1>
-=======
         <h2>zain nasere</h2>
->>>>>>> 2808ff0c6a5d8b0f9b5ff6185c33900412da2cdd
+        junaid
     </div>
   )
 }
