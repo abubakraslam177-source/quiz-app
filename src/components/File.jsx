2808ff0c6a5d8b0f9b5ff6185c33900412da2cdd
junaidarshad1122 <jj8286014@gmail.com>
2025-10-24 23:39:48 -0700

    loooora bhara wala

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -6,6 +6,7 @@ return (
         <button type='button'>push</button>
         <input type="text" placeholder='Search' />
 <h1>Practice Git Page</h1>
+<h2>zain naseer and brother</h2>
 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, sed, saepe porro voluptates, explicabo repellendus amet facere dolores necessitatibus harum adipisci rerum veniam excepturi praesentium eos distinctio eaque labore qui.</p>
     </div>
 )
