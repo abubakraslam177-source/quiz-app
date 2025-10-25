a12749383395c1d6dd98b8d9492955776e0850c3
Abubakar <abubakraslam177@gmail.com>
2025-10-25 11:21:10 +0500

    master

---
+++ src/components/File.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const File = () => {
+return (
+    <div>
+<h1>Practice Git Page</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, sed, saepe porro voluptates, explicabo repellendus amet facere dolores necessitatibus harum adipisci rerum veniam excepturi praesentium eos distinctio eaque labore qui.</p>
+    </div>
+)
+}
+
+export default File
