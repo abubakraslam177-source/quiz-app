e12200d57aeb144af98a728faf77231b4802cb5a
junaidarshad1122 <jj8286014@gmail.com>
2025-10-24 23:38:56 -0700

    lllora

--- src/NewFile.jsx
+++ src/NewFile.jsx
@@ -5,6 +5,7 @@ const NewFile = () => {
     <div>
         <h1 style={{backgroundColor:"green"}}>Abubakar Aslam</h1>
         <h1 style={{fontSize:"56px", color:"purple"}}> Muhammad Abdullah Saeed</h1>
+        <h2>zain nasere</h2>
     </div>
   )
 }
