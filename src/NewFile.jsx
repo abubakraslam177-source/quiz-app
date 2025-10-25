7d31dfd45351b8bb0dbd172ed6f35012d770aaa1
abdullah <you@example.com>
2025-10-25 11:33:31 +0500

    made

--- src/NewFile.jsx
+++ src/NewFile.jsx
@@ -4,6 +4,7 @@ const NewFile = () => {
   return (
     <div>
         <h1 style={{backgroundColor:"green"}}>Abubakar Aslam</h1>
+        <h1 style={{fontSize:"56px", color:"purple"}}> Muhammad Abdullah Saeed</h1>
     </div>
   )
 }
